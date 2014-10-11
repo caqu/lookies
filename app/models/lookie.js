25b0cf05d442eacd8eeee43b7b3b78cf247b32e8
Carlos Quesada <quesadac@ae.com>
2014-10-11 12:24:49 -0400

    added bootstrap map to remove 404

--- app/models/lookie.js
+++ app/models/lookie.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 
   url: DS.attr('string'),      // The url to the photo
+  tags: DS.hasMany('tag'),
 
   backgroundUrl: function () {
     return "background-image:url('" + this.get("url") + "')";
--- app/models/tag.js
+++ app/models/tag.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  lookie: DS.belongsTo('lookie'),
+  // product: DS.belongsTo('product'),
+  ratioFromLeft: DS.attr('number'),
+  ratioFromTop:  DS.attr('number')
 });
