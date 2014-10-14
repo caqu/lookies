96936de68a0dc23af3f80002597939ddf8348a77
Carlos Quesada <quesadac@ae.com>
2014-10-14 17:51:54 -0400

    viewport size vs body size

--- app/views/application.js
+++ app/views/application.js
@@ -5,17 +5,38 @@ export default Ember.View.extend({
   init: function () {
     this._super();
     var view = this;
-    $(window).bind('resize', function (){
-      // view.set('viewport', window.innerHeight+""+window.innerWidth);
-      view.set('viewportHeight', window.innerHeight);
-      view.set('viewportWidth', window.innerWidth);
-    });
+    $(document).ready(updateSizes);
+    $(window).bind('resize', updateSizes);
+    $(document.body).bind('resize', updateSizes);
+    
+    function updateSizes () {
+      console.log( "tis", this);
+      view.updateSizes();
+    }
   },
   
+  updateSizes: function () {
+    this.set('viewportHeight', window.innerHeight);
+    this.set('viewportWidth', window.innerWidth);
+    this.set('bodyWidth', document.body.offsetWidth);
+    this.set('bodyHeight', document.body.offsetHeight);    
+  },
+
+  didInsertElement: function () {
+    this.updateSizes();
+  },
+
   viewportHeight: function () {
     return window.innerHeight;
   }.property(),
   viewportWidth: function () {
     return window.innerWidth;
+  }.property(),
+  bodyWidth: function () {
+    return document.body.offsetWidth;
+  }.property(),
+  bodyHeight: function () {
+    return document.body.offsetHeight;
   }.property()
+
 });
