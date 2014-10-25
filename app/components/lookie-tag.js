70f273ae8e39ddbc22ae393dac2723e914514efa
Carlos Quesada <quesadac@ae.com>
2014-10-24 23:18:40 -0400

    adjusted clickables top buttons

--- app/components/lookie-tag.js
+++ app/components/lookie-tag.js
@@ -4,6 +4,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
   styleWithPosition: function() {
     var calcLeft = this.get('leftBlackBarWidth') + this.get('ratioFromLeft') * this.get('lookieWidth'),
         calcTop  = this.get('topBlackBarHeight') + this.get('ratioFromTop' ) * this.get('lookieHeight');
