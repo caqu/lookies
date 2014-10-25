0de2166ad6794407ae00fd18bbbf8e306aa9bf89
Carlos Quesada <quesadac@ae.com>
2014-10-24 23:42:46 -0400

    turned lookie-tag component into plain ol' anchor tag

--- app/components/lookie-tag.js
+++ app/components/lookie-tag.js
@@ -5,16 +5,29 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  styleWithPosition: function() {
-    var calcLeft = this.get('leftBlackBarWidth') + this.get('ratioFromLeft') * this.get('lookieWidth'),
-        calcTop  = this.get('topBlackBarHeight') + this.get('ratioFromTop' ) * this.get('lookieHeight');
+  tagName: 'a',
+  classNames: ['tag', 'glyphicon', 'glyphicon-plus-sign', 'clickable'],
+
+  attributeBindings: ['href', 'style'],
+
+  href: function () {
+    var h = this.get('productHref');
+    h = h? h.get('id') : '';
+    h = decodeURIComponent( h );
+    return h; 
+  }.property('productHref'),
+
+  style: function() {
+    var calcLeft = this.get('leftBlackBarWidth') + this.get('ratioFromLeft') * this.get('lookieWidth') |0,
+        calcTop  = this.get('topBlackBarHeight') + this.get('ratioFromTop' ) * this.get('lookieHeight')|0;
+
 console.log("calcLeft", calcLeft);
     return "left:"+calcLeft+"px;"+"top:"+calcTop+"px";
   }.property('leftBlackBarWidth', 'topBlackBarHeight', 'lookieHeight', 'lookieWidth'),
 
-  click: function () {
-    // TODO prevent error if href is null
-    var href = decodeURIComponent( this.get('href').get('url') );
-    this.sendAction('action', href);
-  }
+  // click: function () {
+  //   // TODO prevent error if href is null
+  //   var href = decodeURIComponent( this.get('href').get('url') );
+  //   this.sendAction('action', href);
+  // }
 });
